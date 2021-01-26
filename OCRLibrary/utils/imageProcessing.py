bda31ab96afcf779a6b67d771e188df2c99dedac
Ben Durston <bengdurston@gmail.com>
2021-01-25 21:01:17 -0500

    Added a few morphological transformations.

--- OCRLibrary/utils/imageProcessing.py
+++ OCRLibrary/utils/imageProcessing.py
@@ -1,5 +1,7 @@
 """
 Image Processing Reference: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_table_of_contents_imgproc/py_table_of_contents_imgproc.html#
+
+Note: To convert an image to binary (black and white) change image to grayscale, then add binary threshold or inverse binary threshold.
 """
 import cv2
 import numpy as np
@@ -273,52 +275,93 @@ def threshold_tozero_inv_otsu(img, max_thresh):
 """
 Image blurring/Image Smoothing
 """
-def blurring_averaging(img, kernal):
+def blurring_averaging(img, kernel):
     """
     """
-    return cv2.blur(img, kernal)
+    return cv2.blur(img, kernel)
 
-def blurring_gaussian(img, kernal, other):
+def blurring_gaussian(img, kernel, other):
     """
     """
-    return cv2.GaussianBlur(img, kernal, other)
+    return cv2.GaussianBlur(img, kernel, other)
 
 """
 Structuring Element
 """
-def get_rect_kernal(kernal_size):
+def get_rect_kernel(kernel_size):
     """
     Purpose:
-        Gets a rectangular shaped kernal
+        Gets a rectangular shaped kernel
     Args:
-        kernal_size - tuple: size of the kernal.
+        kernel_size - tuple: size of the kernel.
     Returns:
-        2D array representing a rectangular kernal.
+        2D array representing a rectangular kernel.
     """
-    return cv2.getStructuringElement(cv2.MORPH_RECT, kernal_size)
+    return cv2.getStructuringElement(cv2.MORPH_RECT, kernel_size)
 
-def get_ellipse_kernal(kernal_size):
+def get_ellipse_kernel(kernel_size):
     """
     Purpose:
-        Gets a ellipse shaped kernal
+        Gets a ellipse shaped kernel
     Args:
-        kernal_size - tuple: size of the kernal.
+        kernel_size - tuple: size of the kernel.
     Returns:
-        2D array representing an ellipse kernal.
+        2D array representing an ellipse kernel.
     """
-    return cv2.getStructuringElement(cv2.MORPH_ELLIPSE, kernal_size)
+    return cv2.getStructuringElement(cv2.MORPH_ELLIPSE, kernel_size)
 
-def get_cross_kernal(kernal_size):
+def get_cross_kernel(kernel_size):
     """
     Purpose:
-        Gets a rectangular shaped kernal
+        Gets a rectangular shaped kernel
     Args:
-        kernal_size - tuple: size of the kernal.
+        kernel_size - tuple: size of the kernel.
     Returns:
-        2D array representing a rectangular kernal.
+        2D array representing a rectangular kernel.
     """
-    return cv2.getStructuringElement(cv2.MORPH_CROSS, kernal_size)
+    return cv2.getStructuringElement(cv2.MORPH_CROSS, kernel_size)
 
 """
-Morphological transformations: results are best with binary images, specifically white foreground and black background.
+Morphological transformations: results are best with binary images, specifically white foreground and black background. convert_BGR_to_GRAY() -> threshold_binary_inv()
+
+In noise removal erosion is followed by dilation. Erosion removes white noises but shrinks the object. So dilation is performed.
 """
+def morph_erosion(img, kernel, iteration=1):
+    """
+    Erodes the boundarios of foreground objects.
+    """
+    return cv2.erode(img, kernel, iterations= iteration)
+
+def morph_dilation(img, kernel, iteration=1):
+    """
+    Opposite of erosion. A pixel becomes a '1' if at least one pixel under the kernel is '1'.
+    """
+    return cv2.dialte(img, kernel, iterations= iteration)
+
+def morph_opening(img, kernel):
+    """
+    Useful in removing noise. Like erosion then dilation.
+    """
+    return cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+
+def morph_closing(img, kernel):
+    """
+    Opposite to opening.
+    """
+    return cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+
+def morp_gradient(img, kernel):
+    """
+    Will give the object an outline. 
+    """
+    return cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
+
+def morph_top_hat(img, kernel):
+    """
+    """
+    return cv2.morphologyEx(img, cv2.MORPH_TOPHAT, kernel)
+
+def morph_black_hat(img, kernel):
+    """
+    """
+    return cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, kernel)
