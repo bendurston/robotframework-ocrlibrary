c1b49e7e286cd4e0f182b41e72f6cd56459828d6
Ben Durston <bengdurston@gmail.com>
2021-04-08 18:40:45 -0400

    Added two tests for content location keywords.

--- tests/test_content_location.py
+++ tests/test_content_location.py
@@ -5,6 +5,7 @@ Module for Content Location tests.
 import unittest
 import cv2
 from OCRLibrary.OCRLibraryKeywords import Locate_Text_Coordinates, Locate_Multiple_Text_Coordinates, Locate_Text_Bounds, Locate_Multiple_Text_Bounds
+from OCRLibrary.utils.exceptions.exceptions import InvalidImageArgument, ContentNotFound
 
 class TestContentLocation(unittest.TestCase):
     """
@@ -16,12 +17,6 @@ class TestContentLocation(unittest.TestCase):
         """
         self.processed_image = cv2.imread('tests/images/locate_text_coordinates1.png')
         self.text = "Ok"
-        self.expected_x_center = 944
-        self.expected_y_center = 538
-        self.expected_x_bound = 900
-        self.expected_y_bound = 510
-        self.expected_w_bound = 84
-        self.expected_h_bound = 45
 
     def tearDown(self):
         """
@@ -29,28 +24,53 @@ class TestContentLocation(unittest.TestCase):
         """
         del self.processed_image
         del self.text
-        del self.expected_x_center
-        del self.expected_y_center
-        del self.expected_x_bound
-        del self.expected_y_bound
-        del self.expected_w_bound
-        del self.expected_h_bound
 
     def test_01_locate_text_coordinates(self):
         """
         End to end flow of Locate_Text_Coordinates function. All correct arguments.
         """
+        expected_x_center = 944
+        expected_y_center = 538
         actual_x, actual_y = Locate_Text_Coordinates(self.processed_image, self.text)
-        self.assertAlmostEqual(actual_x, self.expected_x_center, delta=5)
-        self.assertAlmostEqual(actual_y, self.expected_y_center, delta=5)
+        self.assertAlmostEqual(actual_x, expected_x_center, delta=5)
+        self.assertAlmostEqual(actual_y, expected_y_center, delta=5)
 
+    def test_02_locate_text_coordinates(self):
+        """
+        Pass in incorrect image
+        """
+        with self.assertRaises(InvalidImageArgument):
+            Locate_Text_Coordinates(None, self.text)
+
+    def test_03_locate_text_coordinates(self):
+        """
+        Text was not found error
+        """
+        pass
 
     def test_01_locate_text_bounds(self):
         """
         End to end flow of Locate_Text_Bounds function. All correct arguments.
         """
+        expected_x_bound = 900
+        expected_y_bound = 510
+        expected_w_bound = 84
+        expected_h_bound = 45
         actual_x, actual_y, actual_w, actual_h = Locate_Text_Bounds(self.processed_image, self.text)
-        self.assertAlmostEqual(actual_x, self.expected_x_bound, delta=5)
-        self.assertAlmostEqual(actual_y, self.expected_y_bound, delta=5)
-        self.assertAlmostEqual(actual_w, self.expected_w_bound, delta=5)
-        self.assertAlmostEqual(actual_h, self.expected_h_bound, delta=5)
+        self.assertAlmostEqual(actual_x, expected_x_bound, delta=5)
+        self.assertAlmostEqual(actual_y, expected_y_bound, delta=5)
+        self.assertAlmostEqual(actual_w, expected_w_bound, delta=5)
+        self.assertAlmostEqual(actual_h, expected_h_bound, delta=5)
+
+    def test_02_locate_text_bounds(self):
+        """
+        Pass in incorrect iamge
+        """
+        with self.assertRaises(InvalidImageArgument):
+            Locate_Text_Bounds(None, self.text)
+
+    def test_03_locate_text_bounds(self):
+        """
+        Text was not found error
+        """
+        pass
