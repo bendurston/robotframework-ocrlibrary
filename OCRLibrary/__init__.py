86b893a742d686008adf5fc2e9ca108f8b1edd08
Ben Durston <bengdurston@gmail.com>
2021-04-27 21:39:57 -0400

    Added documentation for robot libdoc.

--- OCRLibrary/__init__.py
+++ OCRLibrary/__init__.py
@@ -21,41 +21,50 @@ class OCRLibrary(GenericImageTransformationKeywords,
                 ContentValidationKeywords,
                 ContentLocationKeywords):
     """
-    Explanation of the library goes here.
+    OCRLibrary is an image reading and processing library for Robot Framework.
 
-    == About arguments for image processing ==
-    OTSU and thresholding
-    If apply_otsu is true, a tuple will be returned. Index 0 contains the optimal threshold value found by
-            the ostu threshold, and index 1 has the binary image.
+    The OCR component of OCRLibrary utilizes pytesseract, which is a python wrapper for Google's Tesseract OCR.
+    Image processing is done through the opencv-python package.
 
-    === Talk about how some keywords need different kernel sizes, some tuple some ints ===
+    Please note that recogizing all characters and their locations from screenshots is not guaranteed. Although processing the
+    image will increase the chances for the desired characters to be read or located.
 
-    ==== pyt_conf and lang ====
+    == Arguments for image processing keywords ==
+    
+    === Using OTSU For Binary Image Processing ===
+    Enabling otsu ``apply_otsu = True`` for thresholding keywords determines the threshold value automatically.
+    When apply_otsu is true, the image processing keyword will return a tuple. Index 0 contains the optimal threshold 
+    value found by the ostu threshold, and index 1 has the binary image.
 
-    === binary transformation arguments ===
-    img - processed binary image.
-            kernel_size - size of the kernel
-            kernel_type - shape of kernel used. 0 = rectangle, 1 = ellipse, and 2 = cross.
-            iteration - number of iterations to perform on image.
+    List of current keywords that can use otsu: Get Binary Image, Get To Zero Image and, Get Trunc Image
 
-    === generic image transforatmion arguments ====
-    img - processed image.
-            kernel_size - size of the kernel
-            kernel_type - shape of kernel used. 0 = rectangle, 1 = ellipse, and 2 = cross.
-            depth - desired depth of the destination image.
-                when depth=-1, the output image will have the same depth as the source.
+    === Image Transformation Arguments ===
+    Most of the image transformation keywords require a kernel size. All but ``Apply Median Filtering To Image`` take 
+    kernel size as a tuple. These keywords take the kernel size and create a structured element in the shape of either a
+    rectangle, ellipse or a cross. These shapes are chosen by the value passed to ``kernel_type``. 0 will be arectangle, 
+    1 will be an ellipse, and 2 will be a cross.
 
-    ===== Notes on colour bounds =====
-    Note if the image is BGR the bounds
-            must be ints 0 to 255. If the image is HSV the bounds must be a 3 element tuple containing ints 0 to 255.
-            Note - Bounds can be of type tuple or list e.g - (blue, green, red), (hue, saturation, value) or [blue, green, red], [hue, saturation, value].
-        Args:
-            processed_img - the processed image.
-            lower_bound_colour - the lower bound of the colour to exclude from the mask.
-            upper_bound_colour - the upper bound of the colour to exclude from the mask.
-            colour_type - 0 if the processed image is in BGR, 1 if the processed image is in HSV.
+    For the keywords that require an ``iteration`` value - iteration is the number of times the transformation is performed
+    on the image.
 
-    ==== about valid images to read and valid image formats to save processed images. ====
+    For the keywords that require a ``depth`` value - depth represents the desired depth of the destination image. 
+    When ``depth=-1`` the output image will have the same depth as the source.
+
+    === Pytesseract Config and Language Arguments ===
+    Please see [https://github.com/bendurston/robotframework-ocrlibrary#custom-configurations-for-reading-images |the OCRLibrary README.md] for an in depth explanation of the ``pyt_conf`` argument.
+    
+    List of [https://tesseract-ocr.github.io/tessdoc/Data-Files#data-files-for-version-400-november-29-2016 |supported languages] as of Tesseract OCR version 4.0.0, along with language codes used
+    for the ``lang`` argument.
+    
+    == Masking Colours ==
+    Users are able to mask (maintain) colours that exist within the provided upper and lower bounds. A BGR or HSV image can be
+    used for either ``Mask Colour`` or ``Mask Colours``. Bounds can be either a list of a tuple, and each index must be of type int.
+    Representation of BGR and HSV bounds respectively: (blue value, green value, red value), (hue value, saturation value, brightness value).
+
+    == Reading and Saving Images ==
+    Please see the list of the following [https://docs.opencv.org/master/d4/da8/group__imgcodecs.html#ga288b8b3da0892bd651fce07b3bbd3a56 |formats that are supported] for image reading.
+    
+    Please see the [https://docs.opencv.org/master/d4/da8/group__imgcodecs.html#gabbc7ef1aa2edfaa87772f1202d67e0ce |list of exceptions] for saving an image.
     """
 
     def __init__(self):
