1b303e1a325fec096474e6ecc27b51d2b2c118ca
Ben Durston <bengdurston@gmail.com>
2021-04-27 20:40:36 -0400

    Added pypi setup files.

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as f:
-    long_desc = f.read()
+    long_description = f.read()
 
-"""
-TODO - reference: https://packaging.python.org/tutorials/packaging-projects/
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    name="robotframework-OCRLibrary-bendurston",
+    version="1.0.0",
+    author="Ben Durston",
+    author_email="bengdurston@gmail.com",
+    description="A robot framework library that utilizes OpenCV image processing and pytesseract OCR.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/bendurston/robotframework-ocrlibrary",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
 )
-"""
